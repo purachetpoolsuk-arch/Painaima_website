de4c8886a746c0519aaf83f7697e1b4cb06c52c0
purachetpoolsuk-arch <purachetpoolsuk@gmail.com>
2026-09-02 00:52:01 +0700

    fix(vercel): add --break-system-packages and set staticfiles distDir

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "Building project packages..."
-python3 -m pip install -r requirements.txt
-echo "Migrating database..."
-python3 manage.py migrate --noinput
+python3 -m pip install -r requirements.txt --break-system-packages
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
 echo "Build complete!"
